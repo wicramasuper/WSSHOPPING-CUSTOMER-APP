ac63f1b19296bf3bae4387f11b2763ca444eab3d
Sachinikuruppu <sachiii.kuruppu@gmail.com>
2021-09-07 20:53:07 +0530

    basic configuration

--- server.js
+++ server.js
@@ -1,23 +1,37 @@
 const express = require('express');
-const app = express();
+
 const mongoose = require('mongoose');
+const bodyParser= require("body-parser");
+const cors= require("cors");
+const dotenv= require("dotenv");
+const app = express();
 
 require('dotenv').config();
 
-const port = process.env.PORT || 9000;
+const PORT = process.env.PORT || 9000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+//const URL= process.env.MONGO_URL;
 
 mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser:true,
     useCreateIndex:true,
-    useUnifiedTopology:true
+    useUnifiedTopology:true,
+    useFindAndModify:false,
 }).then(()=>{
     console.log("database connection established");
 });
 
-app.listen(port,()=>{
-    console.log(`server is running on port ${port}`);
-});
+//shopping cart
+const shoppingroute= require("./routes/customer-cart/shoppingcart.js");
 
+app.use("/shoppingcart",shoppingroute); 
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`)
+})
 
 //demo changing
 
